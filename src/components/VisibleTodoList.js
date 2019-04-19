bf64ecb117709302d70da41b3a530a63fe383c7f
Artem Zimin <atm.zmn95@gmail.com>
2019-04-19 22:03:20 +0300

    added fetching data on route change

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,13 +1,40 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { toggleTodo } from '../actions';
 import { getVisibleTodos } from '../reducers';
 import TodoList from './TodoList';
+import { fetchTodos } from '../api';
+
+// define component to use lifecycle hook
+class VisibleTodoList extends Component {
+    componentDidMount() {
+        fetchTodos(this.props.filter).then(todos => {
+            console.log(this.props.filter, todos);
+        })
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.filter !== prevProps.filter) {
+            fetchTodos(this.props.filter).then(todos => {
+                console.log(this.props.filter, todos);
+            })
+        }
+    }
+
+    render() {
+        return <TodoList {...this.props} />;
+    }
+}
 
 // maps store state to props of todo component
-const mapStateToTodoListProps = (state, { match }) => ({
-    todos: getVisibleTodos(state, match.params.filter || 'all'),
-});
+const mapStateToTodoListProps = (state, { match }) => {
+    const filter = match.params.filter || 'all';
+    return {
+        todos: getVisibleTodos(state, filter),
+        filter
+    };
+};
 
 export default withRouter(connect(
     mapStateToTodoListProps,
@@ -15,4 +42,4 @@ export default withRouter(connect(
     // as corresponding action creators
     // we can use configuration object instead
     { onTodoClick: toggleTodo }
-)(TodoList));
+)(VisibleTodoList));
