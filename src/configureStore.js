70e7ca58a56040569162b22674e560d55562acce
Artem Zimin <atm.zmn95@gmail.com>
2019-04-21 19:45:13 +0300

    applied redux middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,49 +1,20 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import todoApp from './reducers';
 
-const logger = (store) => (next) => {
-    if (!console.group) {
-        return next;
-    }
-
-    return (action) => {
-        console.group(action.type);
-        console.log('%c prev state', 'color: gray', store.getState());
-        console.log('%c action', 'color: blue', action);
-        const returnValue = next(action);
-        console.groupEnd(action.type);
-        console.log('%c next state', 'color: green', store.getState());
-        return returnValue;
-    }
-}
-
-const promise = (store) => (next) => (action) => {
-    if (typeof action.then === 'function') {
-        return action.then(next);
-    }
-    return next(action);
-};
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-    middlewares.slice().reverse().forEach(middleware => 
-        store.dispatch = middleware(store)(store.dispatch)
-    );
-};
-
 const configureStore = () => {
-    const store = createStore(
-        todoApp,
-    );
     // pass midlewares in order in which action propogates through the middlewares
     const middlewares = [promise];
     
     if (process.env.NODE_ENV !== 'production') {
-        middlewares.push(logger);
+        middlewares.push(createLogger());
     }
 
-    wrapDispatchWithMiddlewares(store, middlewares)
-
-    return store;
+    return createStore(
+        todoApp,
+        applyMiddleware(...middlewares)
+    );
 }
 
 export default configureStore;
